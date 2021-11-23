fce5ada41e5f2894c93256c69b11f9e3fe45624f
AnasElkalla <79075490+AnasElkalla@users.noreply.github.com>
2021-11-23 09:21:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,40 @@ const container = document.querySelector(".container");
 const slider = document.querySelector("input");
 const trials = document.querySelector(".opportunity");
 const image = document.querySelector(".basket img");
+const startGame = document.querySelector(".basket .btn");
+container.style.cursor = "pointer";
 let start = 0;
 let lives = 10;
+let gameStarted = false;
 trials.textContent = 10;
 basket.style.zIndex = "5";
 let basketPosition;
-
+startGame.addEventListener("click", function () {
+  image.style.visibility = "visible";
+  startGame.style.display = "none";
+  trials.textContent = 10;
+  lives = 10;
+  function clicker() {
+    let click = new audio(click.mp3);
+    click.play();
+  }
+  clicker();
+  container.addEventListener(
+    "mousemove",
+    function (e) {
+      container.style.cursor = "none";
+    },
+    10
+  );
+  gameStarted = true;
+});
 function gameOver() {
   basket.innerHTML = "<h1>GAME OVER</h1>";
   let gameOver = new audio(draw.mp3);
   gameOver.play();
   image.style.visibility = "hidden";
+  trials.textContent = 0;
+  count = count;
 }
 
 slider.oninput = function () {
@@ -55,26 +78,28 @@ document.addEventListener(
 container.addEventListener(
   "mousemove",
   function (e) {
-    if (e.movementX < 0) {
-      start += 5;
-      basket.style.right = `${start}px`;
-      if (start > 450) {
-        basket.style.right = "0px";
-        start = 0;
-      }
-    } else {
-      start -= 5;
-      basket.style.right = `${start}px`;
+    if (gameStarted) {
+      if (e.movementX < 0) {
+        start += 10;
+        basket.style.right = `${start}px`;
+        if (start > 450) {
+          basket.style.right = "0px";
+          start = 0;
+        }
+      } else {
+        start -= 10;
+        basket.style.right = `${start}px`;
 
-      if (start < 0) {
-        basket.style.right = "450px";
-        start = 450;
+        if (start < 0) {
+          basket.style.right = "450px";
+          start = 450;
+        }
       }
+      let basketmeter = function () {
+        basketPosition = basket.getBoundingClientRect();
+      };
+      setInterval(basketmeter, 5);
     }
-    let basketmeter = function () {
-      basketPosition = basket.getBoundingClientRect();
-    };
-    setInterval(basketmeter, 5);
   },
   setInterval(1000)
 );
@@ -177,19 +202,11 @@ const game = function () {
         }
       }
     };
-
-    setInterval(checkPosition, 10);
     trials.textContent = lives;
+    setInterval(checkPosition, 5);
     if (lives === 0) {
       gameOver();
     }
   }
 };
 setInterval(game, 3000);
-container.addEventListener(
-  "mousemove",
-  function (e) {
-    container.style.cursor = "none";
-  },
-  10
-);
